98527e92b312c0e050e0f816e24015754df85e36
ismaelnelro <ismaelnelsonrosas@hotmail.com>
2023-08-07 12:47:56 -0300

    cambios en variables no utlizadas  para ver si afecta en el deploy

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,11 @@
 import { Navigate, Route, Routes } from 'react-router-dom'
 import './App.css'
 import HomePage from './pages/HomePage'
-import { useState } from 'react'
 import AdminPage from './pages/Admin/AdminPage'
 import PowerBy from './assets/components/PowerBy'
 function App() {
 
-  const [isAuthenticted, setisAuthenticted] = useState(true)
+  const isAuthenticted = true;
 
   return (
     <div className='bg-blue-400 max-w-[900px] min-w-[320px] m-auto max-h-[1300px] h-screen min-h-[600px] relative'>
--- src/pages/Admin/AdminPage.tsx
+++ src/pages/Admin/AdminPage.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import LayoutPage from '../../Layout/LayoutPage'
 
 const AdminPage = () => {
