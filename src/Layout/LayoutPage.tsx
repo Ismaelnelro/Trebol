e11db55e9f2665e473400456a4cc89e04efdbf7e
ismaelnelro <ismaelnelsonrosas@hotmail.com>
2023-08-11 20:40:22 -0300

    configuracion panel admin

--- src/Layout/LayoutPage.tsx
+++ src/Layout/LayoutPage.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import avatar from '../assets/avatar1.png'
+// import avatar from '../assets/avatar1.png'
+import avatar from '../assets/avatars/avatar4.jpg'
 import '../styles/avatar.css'
 
 const LayoutPage = ({ children }: { children: React.ReactNode }) => {
@@ -10,13 +11,13 @@ const LayoutPage = ({ children }: { children: React.ReactNode }) => {
   }
 
   return (
-    <div className='max-w-[900px] min-w-[320px] max-h-[1000px] h-screen min-h-[600px] flex flex-col items-center gap-4'>
-      <div className='w-full h-auto  flex justify-center p-3' onClick={superUsuario}>
-        <img id='avatar_perfil' src={avatar} alt="avatar perfil" />
+    <div className='max-w-[900px] min-w-[320px] max-h-[1600px]  min-h-[600px] flex flex-col items-center gap-4 relative'>
+      <div className='w-full h-[10rem]  flex justify-center p-3' onClick={superUsuario}>
+        <img id='avatar_perfil' src={avatar} alt="avatar perfil" className='w-full h-full' />
       </div>
 
       <div className='w-full h-8 text-center font-bold '>
-        <p>{name ? '@' + name : "@ismaeltechs"}</p>
+        <p>{name ? '@' + name : "@nametechs"}</p>
       </div>
 
       {children}
